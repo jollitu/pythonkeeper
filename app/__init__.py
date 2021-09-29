09cc6a54c4f317e1477b768f9e81d474f1819360
Jollitu <jollygrave@gmail.com>
2021-09-28 19:53:28 -0600

    Still working on validation + login

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+from app import routes, models
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+# Init Flask
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# Init SQLAlchemy
+db = SQLAlchemy(app)
+
+# Init Flask-Login
+login = LoginManager(app)
--- app/config.py
+++ app/config.py
@@ -1,5 +1,14 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     # TODO don't leave the "test" option in place, lol
     SECRET_KEY = os.environ.get('SECRET_KEY') or "test"
+
+    # TODO
+    DB_URI = os.environ.get("DB_URL") or \
+        'sqlite:///' + os.path.join(basedir, "app.db")
+
+    # TODO this will probably be enabled later on
+    TRACK_MODS = False

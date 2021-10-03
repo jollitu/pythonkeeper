2214607233b64e4937b6a658f3c17eb85737368f
Jollitu <jollygrave@gmail.com>
2021-10-02 21:50:47 -0600

    More login and validation stuff...a bit of a busy night, so not much done

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect
-from flask_login import current_user
+from flask_login import current_user, login_user
 
 from app import app, db
 from app.forms import LoginForm
+from app.models import User
 
 
 # TODO return an object for the front end to consume
@@ -17,6 +18,12 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(user.password_hash):
+            # TODO decide what's most convenient to pass to the front
+            return ""
+        login_user(user, remember=True)
+
         return "{\"username\": " + current_user.__repr__ + "}"
 
     return "{}"
@@ -30,7 +37,10 @@ def logout(username):
 @app.route("/register", methods=["POST"])
 def register(username, password, email):
     user = User(username, password, email)
+    valid_email = user.validate_email(email)
+
+
     db.session.add(user)
     db.session.commit()
 
-    return "{}"
+    return user.__repr__()
